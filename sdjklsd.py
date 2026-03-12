5fcfeb98088e9acedb6be1dfdd65eca69da72f8f
RexxaIsTooRusty <01723790201a@gmail.com>
2026-03-12 14:52:23 +0600

    Add files via upload

---
+++ sdjklsd.py
@@ -0,0 +1,18 @@
+import math
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius * self.radius
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+
+r = float(input("Enter radius: "))
+c = Circle(r)
+
+print("Area of circle:", c.area())
+print("Perimeter of circle:", c.perimeter())
